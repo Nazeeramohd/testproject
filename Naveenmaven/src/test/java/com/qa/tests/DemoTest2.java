e1692a735e31240982bf8f212b47dbae218bfb6e
Nazeeramohd <shaiknazeera@gmail.com>
2019-04-10 16:21:13 +0400

    pushing first test code

---
+++ Naveenmaven/src/test/java/com/qa/tests/DemoTest2.java
@@ -0,0 +1,29 @@
+package com.qa.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+	
+	@Test
+	public void sum() {
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+@Test
+	public void mul() {
+		int a=10;
+		int b=20;
+		Assert.assertEquals(200, a*b);
+	}
+	
+@Test
+	public void div() {
+		int a=10;
+		int b=20;
+		Assert.assertEquals(2, b/a);
+	}
+
+	
+}
